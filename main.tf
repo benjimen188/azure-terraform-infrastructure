ecf5729ddfd1a12aa8c4068dc21d64180c993034
benjimen188 <153070669+benjimen188@users.noreply.github.com>
2023-12-26 19:55:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.prefix}-vnet"
+  name                = "${var.prefix}-VNet"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -45,7 +45,7 @@ resource "azurerm_subnet" "internal" {
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "${var.prefix}-nic"
+  name                = "${var.prefix}-NIC"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -56,20 +56,19 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-resource "azurerm_virtual_machine" "nginx" {
-  name                  = "myVM"
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "${var.prefix}-vm"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.my_nic.id]
-  vm_size               = "Standard_DS1_v2"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_D1_v2"
 
   storage_image_reference {
-    publisher = "bitnami"
-    offer     = "nginxstack"
-    sku       = "16.04-LTS"  
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
     version   = "latest"
   }
- 
   storage_os_disk {
     name              = "myosdisk1"
     caching           = "ReadWrite"
